770c009ff8c17951f1ebfb0176d7c6d67f8c2214
Chirag Bhardwaj <chirag_bhardwaj@student.uml.edu>
2018-12-09 20:31:50 -0500

    Made Changes to the main file

--- src/org/chirag/spark/sentiment/TweetSentimentAnalyzer.scala
+++ src/org/chirag/spark/sentiment/TweetSentimentAnalyzer.scala
@@ -30,7 +30,6 @@ object TweetSentimentAnalyzer {
 
   def main(args: Array[String]): Unit = {
     val ssc = StreamingContext.getActiveOrCreate(createSparkStreamingContext)
-    val simpleDateFormat = new SimpleDateFormat("EE MMM dd HH:mm:ss ZZ yyyy")
 
     LogUtils.setLogLevels(ssc.sparkContext)
 
@@ -52,47 +51,21 @@ object TweetSentimentAnalyzer {
         status.getUser.getScreenName,
         tweetText,
         corenlpSentiment,
-        mllibSentiment,
-        status.getGeoLocation.getLatitude,
-        status.getGeoLocation.getLongitude,
-        status.getUser.getOriginalProfileImageURL,
-        simpleDateFormat.format(status.getCreatedAt))
+        mllibSentiment)
     }
 
     val oAuth: Some[OAuthAuthorization] = OAuthUtils.bootstrapTwitterOAuth()
     val rawTweets = TwitterUtils.createStream(ssc, oAuth)
 
-    if (PropertiesLoader.saveRawTweets) {
-      rawTweets.cache()
-
-      rawTweets.foreachRDD { rdd =>
-        if (rdd != null && !rdd.isEmpty() && !rdd.partitions.isEmpty) {
-          saveRawTweetsInJSONFormat(rdd, PropertiesLoader.tweetsRawPath)
-        }
-      }
-    }
     val DELIMITER = "¦"
     val tweetsClassifiedPath = PropertiesLoader.tweetsClassifiedPath
-    val classifiedTweets = rawTweets.filter(hasGeoLocation)
-      .map(predictSentiment)
+    val classifiedTweets = rawTweets.map(predictSentiment)
 
     classifiedTweets.foreachRDD { rdd =>
       if (rdd != null && !rdd.isEmpty() && !rdd.partitions.isEmpty) {
-        saveClassifiedTweets(rdd, tweetsClassifiedPath)
 
         rdd.foreach {
-          case (id, screenName, text, sent1, sent2, lat, long, profileURL, date) => {
-            val sentimentTuple = (id, screenName, text, sent1, sent2, lat, long, profileURL, date)
-            // TODO -- Need to remove this and use "Spark-Redis" package for publishing to Redis.
-            // TODO -- But could not figure out a way to Publish with "Spark-Redis" package though.
-            // TODO -- Confirmed with the developer of "Spark-Redis" package that they have deliberately omitted the method for publishing to Redis from Spark.
-            val jedis = new Jedis("localhost", 6379)
-            val pipeline = jedis.pipelined
-            val write = sentimentTuple.productIterator.mkString(DELIMITER)
-            val p1 = pipeline.publish("TweetChannel", write)
-            //println(p1.get().longValue())
-            pipeline.sync()
-          }
+      println("Tweet:" + v._1 + " MLlib Analysis : " + mllibSentiment + " and " + "StanfordCoreNLP : " + corenlpSentiment);
         }
       }
     }
@@ -106,8 +79,6 @@ object TweetSentimentAnalyzer {
       .setAppName(this.getClass.getSimpleName)
       // Use KryoSerializer for serializing objects as JavaSerializer is too slow.
       .set("spark.serializer", classOf[KryoSerializer].getCanonicalName)
-      // For reconstructing the Web UI after the application has finished.
-      .set("spark.eventLog.enabled", "true")
       // Reduce the RDD memory usage of Spark and improving GC behavior.
       .set("spark.streaming.unpersist", "true")
 
@@ -115,48 +86,13 @@ object TweetSentimentAnalyzer {
     ssc
   }
 
-  def saveClassifiedTweets(rdd: RDD[(Long, String, String, Int, Int, Double, Double, String, String)], tweetsClassifiedPath: String) = {
-    val now = "%tY%<tm%<td%<tH%<tM%<tS" format new Date
-    val sqlContext = SQLContextSingleton.getInstance(rdd.sparkContext)
-    import sqlContext.implicits._
-    val classifiedTweetsDF = rdd.toDF("ID", "ScreenName", "Text", "CoreNLP", "MLlib", "Latitude", "Longitude", "ProfileURL", "Date")
-    classifiedTweetsDF.coalesce(1).write
-      .format("com.databricks.spark.csv")
-      .option("header", "true")
-      .option("delimiter", "\t")
-      .option("codec", classOf[GzipCodec].getCanonicalName)
-      .save(tweetsClassifiedPath + now)
-  }
-
-  def saveRawTweetsInJSONFormat(rdd: RDD[Status], tweetsRawPath: String): Unit = {
-    val sqlContext = SQLContextSingleton.getInstance(rdd.sparkContext)
-    val tweet = rdd.map(status => jacksonObjectMapper.writeValueAsString(status))
-    val rawTweetsDF = sqlContext.read.json(tweet)
-    rawTweetsDF.coalesce(1).write
-      .format("org.apache.spark.sql.json")
-      // Compression codec to compress when saving to file.
-      .option("codec", classOf[GzipCodec].getCanonicalName)
-      .mode(SaveMode.Append)
-      .save(tweetsRawPath)
-  }
 
   def replaceNewLines(tweetText: String): String = {
     tweetText.replaceAll("\n", "")
   }
 
-  /**
-    * Checks if the tweet Status is in English language.
-    * Actually uses profile's language as well as the Twitter ML predicted language to be sure that this tweet is
-    * indeed English.
-    *
-    * @param status twitter4j Status object
-    * @return Boolean status of tweet in English or not.
-    */
   def isTweetInEnglish(status: Status): Boolean = {
     status.getLang == "en" && status.getUser.getLang == "en"
   }
 
-  def hasGeoLocation(status: Status): Boolean = {
-    null != status.getGeoLocation
-  }
 }
